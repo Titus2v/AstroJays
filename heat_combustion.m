602462402aea83a85357de6d3c11a60254aba8af
Fanuel <fmammo1@jh.edu>
2021-03-16 20:56:51 -0400

    Corrected the Heat of combustion to the heat of tempreture change and added the heats of formations for some reactants

--- heat_combustion.m
+++
@@ -1,6 +0,0 @@
-function [heat_combust] = heat_combustion(T, a1,a2, a3, a4, a5, b1)
-% Calculates the Heat of Combustion of N2
-%   
-heat_combust = 8.314 * T * (a1 + a2*(T/2) + a3*(T^2/3) + a4 * (T^3/4) + a5 * (T^4/5) + b1/T);
-end
-
